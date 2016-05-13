78f23d3d3b739c88fbb3ab9ee2c5e49bde2cae9a
Brad Hudson <bradley.hudson@careerbuilder.com>
2016-05-13 12:46:32 -0400

    routes and tweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,22 +6,12 @@ class HomeController < ApplicationController
   private
 
   def get_tweets(user_name)
-    binding.pry
     get_all_tweets(user_name)
   end
 
-  def collect_with_max_id(collection=[], max_id=nil, &block)
-    response = yield(max_id)
-    collection += response
-    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-  end
-
   def get_all_tweets(user)
-    collect_with_max_id do |max_id|
-      options = {count: 10, include_rts: true}
-      options[:max_id] = max_id unless max_id.nil?
-      client.user_timeline(user, options)
-    end
+    options = {count: 10}
+    client.user_timeline(user, options)
   end
 
   def client
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  get '/' => 'home#index'
+  get '/index' => 'home#index'
+  post '/' => 'home#index'
+  post '/index' => 'home#index'
 
   root 'home#index'
 end
