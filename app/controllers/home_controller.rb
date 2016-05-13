5ca4af6f873bc7d4b844c7013cfd9939ff72a183
Brad Hudson <bradley.hudson@careerbuilder.com>
2016-05-13 12:59:15 -0400

    Working boom

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,16 @@ class HomeController < ApplicationController
   private
 
   def get_tweets(user_name)
-    get_all_tweets(user_name)
+    begin
+      get_all_tweets(user_name) unless user_name.blank?
+    rescue Twitter::Error::TooManyRequests => e
+      flash[:notice] = "Oops! Too many requests to Twitter. Come Back later."
+      nil
+    end
   end
 
   def get_all_tweets(user)
-    options = {count: 10}
+    options = {count: 10, include_rts: false}
     client.user_timeline(user, options)
   end
 
